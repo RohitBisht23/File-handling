129ff1d7b786502b5d866578197af5a655fd7206
Rohit Bisht <127304460+RohitBisht23@users.noreply.github.com>
2024-02-07 15:11:23 +0530

    Add files via upload

---
+++ files.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+
+st.subheader('Uploading the csv files ')
+
+df = st.file_uploader('Uploading the scv file', type=['csv','xlsx'])
+
+# if(df is not None) :
+#     st.dataframe(df)
+st.subheader('Loading the csv files ')
+df = pd.read_csv('C:\\Users\\ROHIT\\Downloads\\apparel.csv', encoding='latin1')
+if df is not None:
+    st.table(df.head())
+
+
+#Image uploading
+st.subheader('Dealing with images')
+img = st.file_uploader('Uploading the scv file', type=['png','jpg','jpeg'])
+if img is not None:
+    st.image(img)
+
+st.image("C:\\Users\\ROHIT\\OneDrive\\Pictures\\porfolio.jpg")
+    
+
+#Working with video file
+st.subheader('Working with video file')
+video_file = st.file_uploader('Upload the video :',type=['mkv','mp4'])
+if video_file is not None:
+    st.video(video_file)
+
+st.video("C:\\Users\\ROHIT\\Videos\\Captures\\WhatsApp Video 2023-10-18 at 23.31.40_572fe237.mp4")
+
+
+#Working with audio file
+st.subheader('Working with audio file')
+audio_file = st.file_uploader('Upload the video :',type=['mp3','wva'])
+if video_file is not None:
+    st.video(audio_file)
